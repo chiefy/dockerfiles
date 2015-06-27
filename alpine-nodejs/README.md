fc232b8be04f0f6a6b36496497f35a81ac11bade
Christopher "Chief" Najewicz <ext.cnajewicz@riotgames.com>
2015-06-27 11:46:27 -0400

    Adding alpine-nodejs w/ alpine:latest and nodejs 0.12.2

---
+++ alpine-nodejs/README.md
@@ -0,0 +1,14 @@
+# alpine-nodejs
+This is a Docker image with Alpine Linux latest running NodeJS 0.12.2
+
+# Usage
+```bash
+
+docker run \
+	--rm \
+	--name alpine-nodejs \
+	chiefy/alpine-nodejs:0.12.2 \
+	node -v
+
+# v0.12.2
+```
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FOLDER=${1:?"Enter a dockerfile folder."}
-VERSION=$2
+VERSION=${2:?"Enter a version of the docker image."}
 
 if [[ ! -d ${FOLDER} ]]; then
 	echo "Could not find folder $(pwd)/$FOLDER, exiting."; exit 1
