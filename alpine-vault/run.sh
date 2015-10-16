4175fd2e700ba447effbed655e2566256537064c
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2015-10-16 11:54:37 -0400

    Fix vault run script

--- alpine-vault/run.sh
+++ alpine-vault/run.sh
@@ -5,7 +5,7 @@ VAULT_CONFIG=${1:?"Enter an absolute path to your vault config HCL file."}
 docker run \
 	--rm \
 	--name vault-dev \
-	--memory-swap -1 \
+	--memory-swap 0 \
 	-v "$VAULT_CONFIG:/etc/vault.hcl" \
 	-p 8200:8200 \
-	chiefy/alpine-vault:0.2.0
+	chiefy/alpine-vault:latest
