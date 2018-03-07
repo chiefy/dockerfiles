d37e8e35dba3f89ad0fae7e8c135fcf7392232f1
Christopher Najewicz <chief@beefdisciple.com>
2018-03-07 09:28:12 -0500

    Docker doesn't like .profile for some reason

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ FOLDER=${1:?"Enter a dockerfile folder."}
 VERSION=${2:?"Enter a version of the docker image."}
 
 if [[ ! -d ${FOLDER} ]]; then
-	echo "Could not find folder $(pwd)/$FOLDER, exiting."; exit 1
+	echo "Could not find folder $(pwd)/$FOLDER, exiting."
+	exit 1
 fi
 
-docker build -t chiefy/$FOLDER:$VERSION $FOLDER
+pushd ${FOLDER} && docker build -t chiefy/$FOLDER:$VERSION ./
+popd
